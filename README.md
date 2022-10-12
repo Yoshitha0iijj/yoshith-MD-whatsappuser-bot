52771ecdc101d0796ab1c5e78c4744dba7e63b58
Yoshitha0iijj <114879837+Yoshitha0iijj@users.noreply.github.com>
2022-10-12 20:03:05 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+##                       𝐲𝐨𝐬𝐡𝐢𝐭𝐡𝐚-𝐌𝐃 𝐰𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐮𝐬𝐞𝐫 𝐛𝐨𝐭
+
+<p align = center>   <img src="https://telegra.ph/file/ba49dade5e4085e40e7ab.jpg" alt="GIF" width="250" height="250"/> </p>
+
+<p align  = center> <a href="#"><img title="yoshitha-MD" src="https://img.shields.io/badge/yoshitha-MD WhatsApp Bot-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a> </p>
+
+## QR Link 
+https://replit.com/@darkalphaxteam/CYBER-X-MD-SCANNER?output%20only=1&lite=1#index.js
+## Deploy Link Template 
+```https://dashboard.heroku.com/new?template=https://github.com/Yoshitha0iijj/yoshitha-MD```
+-----------------
+-----------------
+
+## In some cases, the bot may stop working automatically as soon as the button issue is resolved. So, if it happens, turn the restart Bot. I will find a solution soon.
+## බටන් ගැටලුව විසදීමත් සමගම සමහර අවස්තාවලදි බොට් ක්‍රියාකාරිත්වය ඉබේ නැවතිය හැකිය. එනිසා එසෙ වුවහොත් නැවත් බොට් On කරන්න .ඉක්මනින් පිලියමක් සොයා දෙන්නම්.
+
+## STEPS
+<br>
+
+1. Click [SCAN](https://replit.com/@darkalphaxteam/CYBER-X-MD-SCANNER?v=1) and scan QR through Whatsapp Linked Devices Option in Your WhatsApp App.
+
+2. You will get a long text that needs after Frist step (session.json)
+
+3. If You don't have a account in [Heroku](https://signup.heroku.com/), Create a account.
+
+4. Click [FORK](https://github.com/darkalphaxteam/Cyber-X-Beta-Release/fork)
+5. Upload Session.json You Forked Repo
+6. Deploy And Enjoy
+
+## ALL THE CREDITS GOES TO ORIGINAL REPO OWNER DGNEON (CHEEMS BOT-MD)
+
+## UPDATES
+
+```2022.08.22 - Published```
+
+```2022.08.25 - Tiktok , YT Shorts and Your Tag Error Fixed```
+
+```2022.08.25 - AntiTag Removed```
+
+```2022.09.08 - Costomizeble Footer , APK Downloader , Tiktok Updated , New Alive with Costomizable Alive Massage and etc```
---
+++ settings.js
@@ -0,0 +1,138 @@
+//CYBER-X  DARKALPHAXTEAM ◎◎◎ https://github.com/darkalphaxteam\\
+//══════════════THANKS FOR CHEEMS BOT MD (DGNEON)═ALL THE CREADITS GOES TO HIM═════════════\\
+// EXAM ඉවර උනාම ඉතුරු ටික දෙන්නම් 😓😓😓 GOOD LUCK ALL\\
+const fs = require('fs')
+const chalk = require('chalk')
+
+//API WEBSITE \\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//API KEY\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'your key',
+}
+
+
+//AUTO FUNCTIONS ( 📵 DONT EDIT 📵 ) ITS FOR NEXT UPDATE
+
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = false //auto reading in gc (true to on, false to off)
+global.autobio = false  // auto bio system checking on Developing Leval
+global.autoRecord = false //auto recording (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
+
+//VIDEO MENU 
+global.vidmenu = fs.readFileSync("./LocalData/theme/cyber.mp4") //gif and video menu
+
+//DOCUMENT VARIANTS
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//OWNER CONTACT
+global.vcardowner = ['94775013191'] //ur owner number
+global.ownername = "𓂀 𝓎𝑜𝓈𝒽𝒾𝓉𝒽𝒶-𝒹𝒾𝓁𝓂𝒾𝓉𝒽 𓂀" //ur owner name
+global.ytname = "YT: TGM Arcade" //ur yt chanel name
+global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
+global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+
+//BOT COSTOMIZEDTATION
+global.owner = ['94775013191']
+global.ownertag = '94775013191' //ur tag number
+global.botnumber = '94775013191' //Add your Bot Number For Log
+global.botname = '▂▃▅▇█▓▒░𝚓𝚞𝚙𝚒𝚝𝚎𝚛-𝙼𝙳░▒▓█▇▅▃▂' //ur bot name
+global.ig = "https://www.youtube.com/channel/UCQqumuCQvLMwnZsIKyQWjhw/featured" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.youtube.com/channel/UCQqumuCQvLMwnZsIKyQWjhw/featured" //ur website to be displayed
+global.sc = 'https://github.com/Yoshitha0iijj/Cyber-X-Beta-Release' //script link
+global.linkz = 'https://www.youtube.com/channel/UCQqumuCQvLMwnZsIKyQWjhw/featured'
+global.reactmoji = "😘" //ur menu react emoji
+global.themeemoji = "📩" //ur theme emoji
+global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
+global.author = "DARK ALPHA" //ur sticker watermark author
+global.footer = "® yoshitha dilmith"
+global.aliveimg ="https://telegra.ph/file/15af5e9baa78e9a72c6f0.jpg"
+global.allmenuimg ="https://telegra.ph/file/15ed5fbb60e5c6ea0243f.jpg"
+global.welcomeimg= "https://telegra.ph/file/e314944476ce5f30598e9.jpg"
+global.gbyeimg = "https://telegra.ph/file/6965008cd3ca9b14d2e18.jpg"
+global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions  i'm ꧁༺yoshitha dilmith ༻꧂"
+
+//BOT MEDIAS
+global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur error pic
+global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
+
+//DATA BASED
+
+global.premium = ['94775013191'] //ADD YOU NUMBER
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+//OTHERS
+global.sessionName = 'session'
+global.antitags = true
+global.prefa = ['','!','.']
+global.sp = '⭔'
+global.mess = {
+    success: '✉️✏️ _Done_',
+    admin: '✉️✏️ _This Feature Is Only For Admin!_',
+    botAdmin: '✉️✏️ _Bot Must Be Admin First!_',
+    owner: '✉️✏️ _This Feature Is Only For Owner!_',
+    group: '✉️✏️ _Feature Used Only For Groups!_',
+    private: '✉️✏️ _Features Used Only For Private Chat!_',
+    bot: '✉️✏️ _This Feature Is Only For Bot_',
+    wait: '✉️✏️ _Processing Please Wait..._',
+    linkm: '📧✏️ _Where is the link?_',
+    error: '✉️🤕 ```Error!!```',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+    banChat: 'The bot was banned in this group, please contact the owner to unban'
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./LocalData/theme/cyber.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
